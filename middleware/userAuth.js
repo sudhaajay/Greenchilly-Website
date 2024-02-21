efc04ef1b0e175ea57e13bdc6a205873a7856721
Sudha Ajay <kutoospatoos@gmail.com>
2024-02-21 09:17:51 +0530

    change layout

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,46 +1,36 @@
 const User = require("../model/userModel");
 
-const islogin=async(req,res,next)=>{
-    try {
-
-      const userData = await User.findOne({ _id: req.session.user_id });
-  if (req.session.user_id && userData.isAdmin==0 && userData.is_blocked===1 ) {
-
-    next()
-  } else {
-
-    res.redirect('/')
-  
+const islogin = async (req, res, next) => {
+  try {
+    const userData = await User.findOne({ _id: req.session.user_id });
+    if (
+      req.session.user_id &&
+      userData.isAdmin == 0 &&
+      userData.is_blocked === 1
+    ) {
+      next();
+    } else {
+      res.redirect("/");
+    }
+  } catch (error) {
+    console.log(error.message);
   }
+};
 
-
-    } catch(error){
-  console.log(error.message);
+const islogout = async (req, res, next) => {
+  try {
+    const userData = await User.findOne({ _id: req.session.user_id });
+    if (req.session.user_id && userData.isAdmin == 0) {
+      res.redirect("/");
+    } else {
+      next();
     }
-}
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-
-const islogout=async(req,res,next)=>{
-    try {
-     
-      const userData = await User.findOne({ _id: req.session.user_id });
-        if (req.session.user_id && userData.isAdmin==0  ) {
-  
-            res.redirect('/')
-
-        } else{
-       
-          next()
-        }
-      
-
-    } catch(error){
-  console.log(error.message);
-    }
-}
-
-module.exports={
-    islogin,
-    islogout,
-    
-}
+module.exports = {
+  islogin,
+  islogout,
+};
